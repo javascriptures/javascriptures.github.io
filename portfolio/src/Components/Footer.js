b2a45a0f25ffdf9d103e3f7f17adbcb386f7d92a
javascriptures <jtjavascripture@gmail.com>
2020-05-06 14:39:55 -0700

    mobile responsive

--- portfolio/src/Components/Footer.js
+++ portfolio/src/Components/Footer.js
@@ -1,3 +1,24 @@
+import React from 'react';
+import { Navbar, Container, Row, Col} from 'react-bootstrap';
 
-
-<Navbar fixed="bottom" />
+function Footer(){
+    return (
+    <>
+        <div className="footer">
+        <Navbar sticky="bottom" bg="dark" variant="dark" width="100%" expand='xxl'> 
+            <Navbar.Brand>
+                Jordan Taylor
+                </Navbar.Brand>
+                        <span className="footertext">
+                            © Copyright 2020
+                         </span>
+                        <span className="footertext"> Los Angeles, CA</span>
+                        <span className="footertext">
+                        <a href="mailto:jtjavascripture@gmail.com">Contact me!</a>
+                        </span>
+        </Navbar>
+        </div>
+    </>
+        )
+}
+export default Footer;
--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './Header';
 import About from './About';
+import Footer from './Footer';
 
 
 function Home() {
     return (
+        <>
     <div className="slant">
         <Header/>
         <About/>
     </div>
+    <Footer/>
+    </>
 )
 }
 export default Home;
